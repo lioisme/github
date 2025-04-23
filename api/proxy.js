ae689defaf8d02def46d5e2cca3fb0d405ff3f73
lioisme <lioisme@outlook.com>
2025-04-23 13:32:36 +0800

    Update proxy.js
    修改适配环境变量

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://github.com/";//your website url
+  let target = process.env.TARGET_URL || "https://github.com/";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
